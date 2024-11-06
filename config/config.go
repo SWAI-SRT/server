857113c859ec6823b6fd47b2398f16059283a99a
wlstmd <jinwoo8288@gmail.com>
2024-11-06 17:34:53 +0900

    fix: config

--- config/config.go
+++ config/config.go
@@ -1,38 +1,29 @@
 package config
 
 import (
-	"log"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	DBHost       string
-	DBUser       string
-	DBPassword   string
-	DBName       string
-	JWTSecret    string
-	AWSRegion    string
-	S3BucketName string
+    DBHost       string
+    DBUser       string
+    DBPassword   string
+    DBName       string
+    JWTSecret    string
+    AWSRegion    string
+    S3BucketName string
 }
 
 func LoadConfig() (config Config, err error) {
-	err = godotenv.Load(".env")
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-		return
-	}
+    config = Config{
+        DBHost:       os.Getenv("DB_HOST"),
+        DBUser:       os.Getenv("DB_USER"),
+        DBPassword:   os.Getenv("DB_PASSWORD"),
+        DBName:       os.Getenv("DB_NAME"),
+        JWTSecret:    os.Getenv("JWT_SECRET"),
+        AWSRegion:    os.Getenv("AWS_REGION"),
+        S3BucketName: os.Getenv("S3_BUCKET_NAME"),
+    }
 
-	config = Config{
-		DBHost:       os.Getenv("DB_HOST"),
-		DBUser:       os.Getenv("DB_USER"),
-		DBPassword:   os.Getenv("DB_PASSWORD"),
-		DBName:       os.Getenv("DB_NAME"),
-		JWTSecret:    os.Getenv("JWT_SECRET"),
-		AWSRegion:    os.Getenv("AWS_REGION"),
-		S3BucketName: os.Getenv("S3_BUCKET_NAME"),
-	}
-
-	return
+    return
 }
